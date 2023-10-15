9c8c9e0838588deedbe4a15c5ca08d39d008666c
Alexane <trubert.alexane@orange.fr>
2023-10-15 22:29:53 +0200

    WIP Template directive : ngForm, validation

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'playground';
+  @ViewChild('email')
+  emailInput?: ElementRef<HTMLInputElement>;
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
 }
