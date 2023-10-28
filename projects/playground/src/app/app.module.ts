dd6b88946db6e3bb7b921de568cbb06f4b082225
Alexane <trubert.alexane@orange.fr>
2023-10-28 12:03:06 +0200

    WIP: reactif form

--- projects/playground/src/app/app.module.ts
+++ projects/playground/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import {AppInscriptionComponent} from './inscription.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { BannedEmailDirective } from './banned-email.directive';
 import {UniqueEmailValidator} from "./unique-email.directive";
 import {ConfirmPasswordValidator} from "./confirm-password.directive";
 import { ColorPickerComponent } from './color-picker.component';
+import {AppComponent} from "./app.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppInscriptionComponent,
     BannedEmailDirective,
     UniqueEmailValidator,
     ConfirmPasswordValidator,
@@ -19,6 +21,7 @@ import { ColorPickerComponent } from './color-picker.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
